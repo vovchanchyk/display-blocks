bf8b7dca81388150c7e99a0618481d879317f283
Vovchanchyk <oberigfast@gmail.com>
2021-10-27 15:21:28 +0300

    feat(husky): add husky
    add husky and linter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
