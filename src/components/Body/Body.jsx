9f7d6d5ad40a9edb3209ca54ee234cf4e9b75cc6
Vovchanchyk <60846596+vovchanchyk@users.noreply.github.com>
2021-10-27 21:39:26 +0300

    Lint test (#2)
    * feat(src): pull develop from origin
    
    * fix(husky): test husky
    
    * feat: test husky
    
    * feat: test husky
    
    * fix: test husky
    
    * feat(src/husky): i made husky to work
    
    fix: (src) made rebase from develop

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -9,22 +9,22 @@ import styles from './Body.module.css';
 const Body = () => {
   return (
     <>
-    <div className={styles.title}>
-      <Title />
-    </div>
-    <div className={styles.select}>
-      <p>Per page:</p>
-      <select>
-        <option value="15">15</option>
-        <option value="20">20</option>
-      </select>
-    </div>
-    <div className={styles.table}>
-      <Table />
-    </div>
-    <div className={styles.pagination}>
-      <Pagination />
-    </div>
+      <div className={styles.title}>
+        <Title />
+      </div>
+      <div className={styles.select}>
+        <p>Per page:</p>
+        <select>
+          <option value='15'>15</option>
+          <option value='20'>20</option>
+        </select>
+      </div>
+      <div className={styles.table}>
+        <Table />
+      </div>
+      <div className={styles.pagination}>
+        <Pagination />
+      </div>
     </>
   );
 };
