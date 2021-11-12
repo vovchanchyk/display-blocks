23896945f4323406486ddc780b72ee66f2136d56
Vovchanchyk <oberigfast@gmail.com>
2021-11-12 11:44:18 +0200

    feat(vfv): vv
    vv

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
       <div className={styles.footer__links}>
         <a href='#'>Link</a>
         <a href='#'>Link</a>
+        <a href='#'>Link1</a>
+        <a href='#'>Link2</a>
         <a href='#'>Link</a>
-        <a href='#'>Link</a>
-        <a href='#'>Link</a>
-        <a href='#'>Link</a>
+        <a href='#'>Link3</a>
         <a href='#'>Link</a>
         <a href='#'>Link</a>
         <a href='#'>Link</a>
